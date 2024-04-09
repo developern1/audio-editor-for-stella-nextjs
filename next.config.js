889e52308eaf553d22baf3c6a46fe6e7bb660dad
developern1 <developern.com@gmail.com>
2024-04-09 19:45:31 +0900

    Edit nextjs.yml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const prefix =
+  process.env.NODE_ENV === 'production' ? 'https://developern1.github.io/audio-editor-for-stella-nextjs/' : ''
+
 const nextConfig = {
-  output: 'export'
-}
+  output: 'export',
+  assetPrefix: prefix,
+  }
 
 module.exports = nextConfig
